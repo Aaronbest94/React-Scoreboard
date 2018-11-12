0b3bcc790819b26ff3cbffbe1c06ab00e6e4bcf5
Aaron Best <Aaronbest05@gmail.com>
2018-11-11 18:17:32 -0600

    React Scoreboard Application

---
+++ scoreboard/precache-manifest.8e26d0a91be985fa46ba084bd770bb27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc1b41325083169089dd",
+    "url": "/scoreboard/static/js/runtime~main.dc1b4132.js"
+  },
+  {
+    "revision": "636534b05e9f84473e9f",
+    "url": "/scoreboard/static/js/main.636534b0.chunk.js"
+  },
+  {
+    "revision": "412fd7da2cb1e64e6446",
+    "url": "/scoreboard/static/js/1.412fd7da.chunk.js"
+  },
+  {
+    "revision": "636534b05e9f84473e9f",
+    "url": "/scoreboard/static/css/main.c4b5583b.chunk.css"
+  },
+  {
+    "revision": "597dc74f41ef6a203a2f24df023e4944",
+    "url": "/scoreboard/index.html"
+  }
+];
